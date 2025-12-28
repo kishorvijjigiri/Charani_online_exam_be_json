321bc9d565a4f7c984b021f7dd13ace09cc6b53a
mohan2056 <63034821+mohan2056@users.noreply.github.com>
2025-12-28 20:30:31 +0530

    Update allowed origins in CORS configuration

--- src/main/java/com/jobportel/boot/config/CorsConfig.java
+++ src/main/java/com/jobportel/boot/config/CorsConfig.java
@@ -12,7 +12,7 @@ public class CorsConfig implements WebMvcConfigurer {
         registry.addMapping("/api/**")
                 .allowedOrigins(
                     "http://localhost:3000",
-                    "https://charanionlineexamfe.vercel.app"
+                    "https://charanionlineexam.vercel.app"
                 )
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
